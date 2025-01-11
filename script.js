a73952e93d93aaa1ba98330cf4abfe611f23a8d5
yokka361 <e20361@eng.pdn.ac.lk>
2025-01-11 22:28:55 +0530

    web

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add any JavaScript functionality for smooth scrolling or animations if needed
+document.querySelectorAll("a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
